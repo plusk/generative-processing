a920e9361e00919a354c0dbd68b47e4013402111
plusk <andelar96@gmail.com>
2020-07-22 18:09:42 +0200

    worst mess ive ever made

--- serpent/dots.js
+++ serpent/dots.js
@@ -5,14 +5,19 @@ let COLORS;
 let BG;
 let STROKE;
 
-const PALETTE_NAME = "onom";
+const PALETTE_NAME = "monster";
+const MIN_ALPHA = 63;
+const MAX_ALPHA = 255;
 
-const STROKE_WEIGHT = 10;
+const STROKE_WEIGHT = 15;
 const UNIT = 1080 / 3;
-
 const MAGIC = 4.75;
+const ANGELS = [Math.PI / 3.9, Math.PI / 4.5, Math.PI / 5.9, Math.PI / 11];
+const MAGIQUE = [15, 10, 6, 2];
+const ODD = [true, false, false, false];
 
 let scroll = 0;
+const OFFSETS = [];
 
 function preload() {
   PALETTES = loadJSON("palettes.json");
@@ -23,58 +28,114 @@ function setup() {
   cnv.mouseClicked(clickOnSave);
 
   PALETTE = PALETTES[PALETTE_NAME];
-  // const PALETTE_KEYS = Object.keys(PALETTES);
-  // PALETTE = PALETTES[PALETTE_KEYS[(PALETTE_KEYS.length * Math.random()) << 0]];
   COLORS = PALETTE["colors"];
   BG = color(PALETTE["bg"]);
   STROKE = color(random(COLORS));
 
   background(BG);
   fill(BG);
-  //STROKE.setAlpha(5);
+  STROKE.setAlpha(MIN_ALPHA);
   stroke(STROKE);
   strokeWeight(STROKE_WEIGHT);
 
-  //drawingContext.shadowBlur = STROKE_WEIGHT;
-  //drawingContext.shadowColor = random(COLORS);
+  for (let i = 0; i < MAXYNDEX; i++) {
+    OFFSETS.push(round(random(DURATION)));
+  }
 }
 
+let ticker = 0;
+const LAYERS = 4;
+const MAXNDEX = 3;
+const MAXYNDEX = 16;
+const DURATION = 120;
+const HIGHLIGHTED = [];
+
 function draw() {
-  //circle(width / 2, height / 2, 100);
-  // beginShape(); POINTS, LINES, TRIANGLES, TRIANGLE_FAN, TRIANGLE_STRIP, QUADS, QUAD_STRIP
   let odd = false;
   const HEIGHT = height + UNIT;
+  let yndex = 0;
   for (let y = scroll; y <= HEIGHT; y += UNIT / 4) {
+    const WAVE = [];
+    if ((ticker + OFFSETS[yndex % MAXYNDEX]) % DURATION === 0) {
+      HIGHLIGHTED[yndex] = Math.round((LAYERS - 1) * Math.random());
+    }
+    let xndex = 0;
     for (let x = 0; x <= width; x += UNIT) {
+      const FAN = [];
       if (odd) x += UNIT * 0.5;
-      const LAYERS = 4;
-
       for (let l = 0; l < LAYERS; l++) {
-        beginShape(POINTS);
         const diameter = (UNIT * (LAYERS - l + 1)) / LAYERS;
-        stroke(BG);
-        //stroke("#ff0000");
-        arc(x, y, diameter, diameter, PI, TWO_PI);
-        stroke(STROKE);
-        //const MAGIQUE = [28.8, 20, 15.2, 11.4];
-        const MAGIQUE = [28.8, 20, 15.2, 11.4];
-        for (let a = 0; a <= PI / 2; a += (0.5 * PI) / MAGIQUE[l]) {
+        const stepper =
+          (ANGELS[l] / (ODD[l] ? MAGIQUE[l] - 1 : MAGIQUE[l])) * 2;
+        const starter = ODD[l] ? 0 : stepper / 2;
+        const LAYER = [];
+        for (let a = starter; a <= ANGELS[l]; a += stepper) {
           const angle = (TWO_PI / 4) * 3 + a;
-          const xDot = x + (diameter * cos(angle)) / 2;
-          const yDot = y + (diameter * sin(angle)) / 2;
-          vertex(xDot, yDot);
-          const xDottwo = x - (diameter * cos(-angle)) / 2;
-          const yDottwo = y - (diameter * sin(-angle)) / 2;
-          vertex(xDottwo, yDottwo);
-          //line(x, y, xDot, yDot);
+          LAYER.push({
+            x: x + (diameter * cos(angle)) / 2,
+            y: y + (diameter * sin(angle)) / 2,
+          });
+          if (a != 0) {
+            LAYER.push({
+              x: x - (diameter * cos(-angle)) / 2,
+              y: y - (diameter * sin(-angle)) / 2,
+            });
+          }
         }
-        endShape(CLOSE);
+        LAYER.sort((a, b) => a.x - b.x);
+        drawDots(x, y, l, diameter, LAYER, HIGHLIGHTED[yndex], xndex, yndex);
+        FAN.push(LAYER);
       }
+      WAVE.push(FAN);
       if (odd) x -= UNIT * 0.5;
+      xndex++;
     }
     odd = !odd;
+    yndex++;
   }
-  //scroll--;
+  scroll--;
+  ticker++;
+  saveCanvas();
+  if (ticker === 600) {
+    noLoop();
+  }
+}
+
+function drawDots(x, y, l, diameter, dots, HIGHLIGHTED, xndex, yndex) {
+  drawingContext.shadowBlur = 0;
+  stroke(BG);
+  arc(x, y, diameter, diameter, PI, TWO_PI);
+
+  const oddish = yndex % 2;
+
+  const totallayerdots = dots.length * (MAXNDEX + oddish) * 2;
+  const alphaRange = MAX_ALPHA - MIN_ALPHA;
+  const dotoffset = (totallayerdots / (MAXNDEX + 1)) * xndex;
+  const thisticker = ticker + OFFSETS[yndex % MAXYNDEX];
+
+  const thisstroke = color(COLORS[yndex % COLORS.length]);
+
+  beginShape(POINTS);
+  for (let i = 0; i < dots.length; i++) {
+    if (l === HIGHLIGHTED) {
+      const timeFactor = (thisticker % DURATION) / DURATION;
+      const timedot = totallayerdots * timeFactor;
+      const thispos = dotoffset + i + totallayerdots / 2;
+      const yootaro = (2.5067 / 1) * sqrt(TWO_PI);
+      const eeek = Math.exp(
+        -((thispos - timedot * 2) ** 2) / (0.25 * timedot) ** 2
+      );
+      //drawingContext.shadowColor = thisstroke;
+      //drawingContext.shadowBlur = STROKE_WEIGHT;
+      thisstroke.setAlpha(MIN_ALPHA + alphaRange * yootaro * eeek);
+    } else {
+      thisstroke.setAlpha(MIN_ALPHA);
+    }
+    stroke(thisstroke);
+    const dot = dots[i];
+    vertex(dot.x, dot.y);
+  }
+  endShape();
 }
 
 function clickOnSave() {
