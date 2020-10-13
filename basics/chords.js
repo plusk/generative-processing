16d97ce1e262fb72263c873deff87803c05e8ccc
Anders Larsen <andelar96@gmail.com>
2020-10-13 21:06:40 +0200

    Added Hoff-esque random chords

---
+++ basics/chords.js
@@ -0,0 +1,82 @@
+let PALETTES, COLORS, STROKE, BACKGROUNDS, BG;
+
+const EXPORT = false;
+
+const PALETTE_NAME = "mono";
+
+const STROKE_WEIGHT = 1;
+const OPACITY = 0.1;
+const RADIUS = 350;
+
+const IS_RANDOM = false;
+
+const LINE_COUNT = 10;
+const NOISE_SPEED = 0.05;
+
+const RANDOM_BIASED_ANGLE = true;
+let BIASED_ANGLE = Math.PI / 2;
+
+function preload() {
+  PALETTES = loadJSON("palettes.json");
+}
+
+function setup() {
+  if (EXPORT) frameRate(4);
+  if (EXPORT) pixelDensity(1);
+
+  const cnv = createCanvas(1080, 1350); // 1080, 1350
+  cnv.mouseClicked(clickOnSave);
+
+  colorMode(HSL);
+  const PALETTE = PALETTES[PALETTE_NAME];
+  // const PALETTE_KEYS = Object.keys(PALETTES);
+  // PALETTE = PALETTES[PALETTE_KEYS[(PALETTE_KEYS.length * Math.random()) << 0]];
+  COLORS = PALETTE["colors"].map((col) => color(col));
+  BACKGROUNDS = PALETTE["bg"].map((col) => color(col));
+
+  STROKE = random(COLORS);
+  BG = random(BACKGROUNDS);
+
+  STROKE.setAlpha(OPACITY);
+
+  background(BG);
+  stroke(STROKE);
+  strokeWeight(STROKE_WEIGHT);
+
+  BIASED_ANGLE = random(TWO_PI);
+
+  noiseDetail(4, 0.75);
+
+  //drawingContext.shadowBlur = STROKE_WEIGHT;
+  //drawingContext.shadowColor = STROKE;
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+  rotate(BIASED_ANGLE);
+
+  for (let i = 0; i < LINE_COUNT; i++) {
+    let a1, a2;
+
+    if (IS_RANDOM) {
+      a1 = random(TWO_PI);
+      a2 = random(TWO_PI);
+    } else {
+      a1 = TWO_PI * noise(i, frameCount * NOISE_SPEED);
+      a2 = TWO_PI * noise(i + LINE_COUNT, frameCount * NOISE_SPEED);
+    }
+
+    const x1 = RADIUS * cos(a1);
+    const y1 = RADIUS * sin(a1);
+    const x2 = RADIUS * cos(a2);
+    const y2 = RADIUS * sin(a2);
+
+    line(x1, y1, x2, y2);
+  }
+
+  // beginShape(); POINTS, LINES, TRIANGLES, TRIANGLE_FAN, TRIANGLE_STRIP, QUADS, QUAD_STRIP
+}
+
+function clickOnSave() {
+  saveCanvas();
+}
