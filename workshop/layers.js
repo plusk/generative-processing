bbb1ffc8674172759b562c4269d239d33ea65e62
Anders Larsen <andelar96@gmail.com>
2020-10-20 21:40:44 +0200

    Documenting layers

--- workshop/layers.js
+++ workshop/layers.js
@@ -1,4 +1,4 @@
-let PALETTES, COLORS, STROKE, BG;
+let PALETTES, COLORS, FILL, BG;
 
 /* Enable to make a canvas suitable for A2 paper */
 const PRINT_MODE = false;
@@ -7,29 +7,44 @@ const PRINT_MODE = false;
 const RANDOM_PALETTE = false;
 const PALETTE_NAME = "genesis";
 
-const STROKE_WEIGHT = 1;
 const LAYER_COUNT = 10;
-const MAX_RADIUS = 500;
 
-const INVERTED_GRADIENT = false;
-const CAP_LIGHTNESS = false;
+/* The average radius of the full shape */
+const MEAN_RADIUS = 500;
+
+/* Enable strokes on the border of each layer, specify weight if enabled */
 const HAS_STROKE = false;
+const STROKE_WEIGHT = 1;
+
+/* Layers are light to dark (from the center), enable to reverse it */
+const INVERTED_GRADIENT = false;
+
+/* Instead of gradient from light to dark, go from the fill color to dark */
+const CAP_LIGHTNESS = false;
+
+/* The degree to which noise affects the layers */
+/* Low values are blobby, high values are spikey */
+const NOISE_MULTIPLIER = 0.2;
+
+/* The speed at which the layers */
+const NOISE_SPEED = 0.005;
+
+/* Mirror the layers on either axis */
 const SYMMETRICAL_X = false;
 const SYMMETRICAL_Y = false;
 
-/**
- * Background is determined by the gradient, but these flags may override it
- * If both flags are true, palette background will be used
- */
+/* Background is determined by the gradient, but these flags may override it */
+/* If both flags are true, palette background will be used */
 const USE_FILL_AS_BACKGROUND = true;
 const USE_PALETTE_BACKGROUND = false;
 
-const NOISE_AMPLIFIER = 2;
-const NOISE_SPEED = 0.005;
-const SHARPNESS = 0.1;
+/* The amount of points that make up each layer, lower means "pointier" */
+/* For example 3 points mean triangular layers, 4 means squares */
+/* Higher might not be noticable, but will make for smoother borders */
+const POINT_COUNT = 500;
 
-/* Negative speed rotates counter-clockwise */
-const ROTATION_SPEED = 0.05;
+/* Mild rotation, negative speed rotates counter-clockwise */
+const ROTATION_SPEED = 0.001;
 
 /*
 
@@ -57,57 +72,65 @@ function setup() {
   BG = color(PALETTE.bg);
 
   /* Sketch-specific setup */
-  STROKE = random(COLORS);
-
-  HAS_STROKE ? stroke(STROKE) : noStroke();
   strokeWeight(STROKE_WEIGHT);
-  angleMode(DEGREES);
+  FILL = random(COLORS);
+  HAS_STROKE ? stroke(FILL) : noStroke();
 }
 
 function draw() {
+  /* Move coordinate system to center of canvas for easier trigonometry */
   translate(width / 2, height / 2);
+
+  /* Home-made helper function to select background based on config */
   drawBackground();
 
+  /* Because layers are drawn in sequence, draw large layers on bottom first */
   for (let i = LAYER_COUNT; i > 0; i--) {
-    drawLayer((MAX_RADIUS / LAYER_COUNT) * i, i);
+    drawLayer((MEAN_RADIUS / LAYER_COUNT) * i, i);
   }
 }
 
 function drawBackground() {
   if (INVERTED_GRADIENT) {
-    CAP_LIGHTNESS ? background(STROKE) : background(255);
+    CAP_LIGHTNESS ? background(FILL) : background(255);
   } else {
     background(0);
   }
-  USE_FILL_AS_BACKGROUND && background(STROKE);
+  USE_FILL_AS_BACKGROUND && background(FILL);
   USE_PALETTE_BACKGROUND && background(BG);
 }
 
 function drawLayer(r, i) {
+  /* A number from 0 to 1, where the middle layer would have 0.5 */
   const layerColorFactor = INVERTED_GRADIENT
     ? i / LAYER_COUNT
     : 1 - i / LAYER_COUNT;
 
+  /* Adjust saturation and lightness based on the layer factor */
   const fillColor = color(
-    hue(STROKE),
-    layerColorFactor * saturation(STROKE),
-    layerColorFactor * (CAP_LIGHTNESS ? lightness(STROKE) : 100)
+    hue(FILL),
+    layerColorFactor * saturation(FILL),
+    layerColorFactor * (CAP_LIGHTNESS ? lightness(FILL) : 100)
   );
   fill(fillColor);
 
+  /* Iterate through a full circle of angles to make a layer */
   beginShape();
-  for (let a = 0; a < 360; a++) {
+  for (let a = 0; a < TWO_PI; a += TWO_PI / POINT_COUNT) {
+    /* Maybe overcomplicated way of getting x and y coordinate in noise field */
     const xOff = SYMMETRICAL_Y
       ? cos(a)
-      : map(i * SHARPNESS * cos(a) + 1, -1, 1, 1, 1 + NOISE_AMPLIFIER);
+      : map(i * cos(a) + 1, -1, 1, 1, 1 + NOISE_MULTIPLIER);
     const yOff = SYMMETRICAL_X
       ? sin(a)
-      : map(i * SHARPNESS * sin(a) + 1, -1, 1, 1, 1 + NOISE_AMPLIFIER);
+      : map(i * sin(a) + 1, -1, 1, 1, 1 + NOISE_MULTIPLIER);
+
+    /* Get noise based on x, y, and the "time" */
     const noised = noise(xOff, yOff, frameCount * NOISE_SPEED);
 
-    const noisedRadius = noised * r;
-    const x = noisedRadius * cos(a + frameCount * ROTATION_SPEED);
-    const y = noisedRadius * sin(a + frameCount * ROTATION_SPEED);
+    /* Compute the final x and y and set a vertex there for the shape */
+    const x = noised * r * cos(a + frameCount * ROTATION_SPEED);
+    const y = noised * r * sin(a + frameCount * ROTATION_SPEED);
     vertex(x, y);
   }
   endShape(CLOSE);
