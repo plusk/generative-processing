3327f02cfd8b8a42bca1aa0824577a3864f3febd
plusk <andelar96@gmail.com>
2021-03-23 10:49:34 +0100

    Tweaked web-version of fagdag
    Former-commit-id: d73f32e59db2146e452176b821e67fe57bc70589

--- fagdag/web.js
+++ fagdag/web.js
@@ -1,11 +1,14 @@
-let vMax, vMin, PALETTE, BEKK_PALETTE, BLOBS, SPOTS;
+let vMax, vMin, TEXT_SIZE, PALETTE, BEKK_PALETTE, BLOBS, SPOTS;
 
 // How spiky the blobs are
 const BLOB_AMP = 0.5;
-const BLOB_LINE_AMP = 0.75;
+const LINE_AMP = 0.75;
+const SPOT_AMP = 0.5;
 
 // How steep the easing curve is, tends to push blobs to their rMin or rMax
-const BLOB_EASE_STEEPNESS = 3;
+const BLOB_EASE_STEEPNESS = 4;
+const SPOT_EASE_STEEPNESS = 5;
+const LINE_EASE_STEEPNESS = 2;
 
 // The speed at which blobs grow and shrink
 const BLOB_ANIMATION_SPEED = 0.002;
@@ -14,20 +17,19 @@ const BLOB_ANIMATION_SPEED = 0.002;
 const ROTATION_SPEED = 0.002;
 
 // A rough representation of the number of vertices that make up a blob, lower value = more points
-const MAX_POINT_DIST = 10;
-
-// Intrinsically tied to the rMin of central blob
-const TEXT_SIZE = 48;
+const MAX_POINT_DIST = 15;
 
 // The thickness of lines
 const STROKE_WEIGHT = 2;
 
 const RANDOM_COLORS = false;
 
-const SPOT_COUNT = 30;
-const SPOT_SPREAD = 6;
-const SPOT_SLANT = 5;
-const SLOT_APPEAR_NOISE = 0.005;
+const SPOT_COUNT = 40;
+const SPOT_X_SPREAD = 7;
+const SPOT_Y_SPREAD = 0.5;
+const SPOT_SLANT = 2;
+const SPOT_APPEAR_NOISE = 0.005;
+const SPOT_DURATION = 500; // 0 means the spots will always be visible
 
 const MOVE_SPEED = 0.1;
 const MOVE_RADIUS = 100;
@@ -57,6 +59,7 @@ class Blob {
     mAngle = random(TWO_PI),
     rMin,
     rMax,
+    amp = BLOB_AMP,
     fillColor,
     aStart = 0,
     aEnd = TWO_PI,
@@ -74,6 +77,7 @@ class Blob {
     this.mAngle = mAngle;
     this.rMin = rMin;
     this.rMax = rMax;
+    this.amp = amp;
     this.fillColor = fillColor;
     this.aStart = aStart;
     this.aEnd = aEnd;
@@ -83,7 +87,7 @@ class Blob {
     this.disappearTime = disappearTime;
     this.visible = this.disappearTime === 0;
     this.disappearTick =
-      noise(cx * SLOT_APPEAR_NOISE, cy * SLOT_APPEAR_NOISE) *
+      noise(cx * SPOT_APPEAR_NOISE, cy * SPOT_APPEAR_NOISE) *
       this.disappearTime *
       (1 - cy / height);
   }
@@ -111,7 +115,7 @@ class Blob {
     // Might put all of this in the constructor
     const pointCount = max((TWO_PI * this.rMax) / MAX_POINT_DIST, 30);
     const deltaA = this.aEnd - this.aStart;
-    const amp = this.line ? BLOB_LINE_AMP : BLOB_AMP;
+    const amp = this.amp;
 
     const rotation = frameCount * ROTATION_SPEED;
     this.mAngle = (this.mAngle + this.mSpeed) % TWO_PI;
@@ -154,26 +158,165 @@ class Blob {
   }
 }
 
-function windowResized() {
-  vMax = max(width, height);
-  vMin = min(width, height);
+function setupBlobs () {
+  BLOBS = [
+    new Blob({
+      z: 10,
+      cx: 0,
+      cy: height * 0.9,
+      rMin: vMin * 0.1,
+      rMax: vMin * 0.4,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.yellow,
+    }),
+    new Blob({
+      z: 20,
+      cx: width * 0.05,
+      cy: height * 0.2,
+      rMin: vMin * 0.25,
+      rMax: vMin * 0.5,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.pink,
+    }),
+    new Blob({
+      z: 30,
+      cx: 0,
+      cy: height * 0.5,
+      rMin: vMin * 0.1,
+      rMax: vMin * 0.3,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.dark,
+    }),
+    new Blob({
+      z: 40,
+      cx: width * 0.8,
+      cy: height,
+      rMin: vMin * 0.1,
+      rMax: vMin * 0.3,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.white,
+    }),
+    new Blob({
+      z: 50,
+      cx: width * 0.9,
+      cy: height * 0.75,
+      rMin: vMin * 0.2,
+      rMax: vMin * 0.4,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.yellow,
+    }),
+    new Blob({
+      z: 60,
+      cx: width * 1,
+      cy: height * 0.5,
+      rMin: vMin * 0.2,
+      rMax: vMin * 0.5,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.teal,
+    }),
+    new Blob({
+      z: 100,
+      cx: 0,
+      cy: 0,
+      rMin: vMin * 0.2,
+      rMax: vMin * 0.8,
+      aEnd: PI,
+      line: true,
+      amp: LINE_AMP,
+      steepness: LINE_EASE_STEEPNESS
+    }),
+    new Blob({
+      z: 200,
+      cx: width,
+      cy: height,
+      rMin: vMin * 0.25,
+      rMax: vMin * 0.45,
+      aStart: PI,
+      aEnd: PI + HALF_PI,
+      line: true,
+      amp: LINE_AMP,
+      steepness: LINE_EASE_STEEPNESS
+    }),
+    new Blob({
+      z: 300,
+      cx: width,
+      cy: height,
+      rMin: vMin * 0.35,
+      rMax: vMin * 0.55,
+      aStart: PI,
+      aEnd: PI + HALF_PI,
+      line: true,
+      amp: LINE_AMP,
+      steepness: LINE_EASE_STEEPNESS
+    }),
+    new Blob({
+      z: 1000,
+      cx: width / 2,
+      cy: height / 2,
+      rMin: vMin * 0.2,
+      rMax: vMin * 0.4,
+      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.white,
+    }),
+    new Blob({
+      z: 2000,
+      cx: width / 2,
+      cy: height / 2,
+      rMin: vMin * 0.25,
+      rMax: vMin * 0.45,
+      fillColor: PALETTE.dark,
+    }),
+  ];
 
-  //resizeCanvas(1080, 1080);
-  // resizeCanvas(1080, 1920);
-  // resizeCanvas(windowWidth, round(windowWidth / 2.75));
+  SPOTS = [];
+  const SPOT_RMAX = vMin * 0.025;
+  for (let i = 0; i < SPOT_COUNT; i++) {
+    SPOTS.push(
+      new Blob({
+        z: i,
+        cx:
+          width * 0.25 +
+          random(-SPOT_RMAX * SPOT_X_SPREAD, SPOT_RMAX * SPOT_X_SPREAD) -
+          i * SPOT_SLANT,
+        cy: (i * SPOT_RMAX * SPOT_Y_SPREAD),
+        rMin: vMin * 0.005,
+        rMax: SPOT_RMAX,
+        amp: SPOT_AMP,
+        fillColor: RANDOM_COLORS ? randomColor() : PALETTE.blue,
+        animationSpeed: 0,
+        steepness: SPOT_EASE_STEEPNESS,
+        disappearTime: SPOT_DURATION,
+      })
+    );
+    SPOTS.push(
+      new Blob({
+        z: i + SPOT_COUNT,
+        cx:
+          width * 0.75 +
+          random(-SPOT_RMAX * SPOT_X_SPREAD, SPOT_RMAX * SPOT_X_SPREAD) +
+          i * SPOT_SLANT,
+        cy: height - (i * SPOT_RMAX * SPOT_Y_SPREAD),
+        rMin: vMin * 0.005,
+        rMax: SPOT_RMAX,
+        amp: SPOT_AMP,
+        fillColor: RANDOM_COLORS ? randomColor() : PALETTE.blue,
+        animationSpeed: 0,
+        steepness: SPOT_EASE_STEEPNESS,
+        disappearTime: SPOT_DURATION,
+      })
+    );
+  }
+}
+
+function windowResized() {
+  vMin = min(width, height);
+  vMax = max(width, height);
+  resizeCanvas(windowWidth, round(windowWidth / 2.75));
+
+  setupBlobs();
+
+  // Intrinsically tied to the rMin of central blob
+  TEXT_SIZE = vMin * 0.1;
+  textSize(TEXT_SIZE);
 }
 
 function setup() {
   colorMode(HSL);
-  createCanvas(1080, 1080);
-  //createCanvas(1080, 1920);
-  //createCanvas(windowWidth, round(windowWidth / 2.75));
+  createCanvas(windowWidth, round(windowWidth / 2.75));
 
-  windowResized();
-
-  strokeWeight(STROKE_WEIGHT);
-
-  /* Get colors from the palettes */
   PALETTE = {
     bg: color("#fff0ac"),
     dark: color("#1c2c3c"),
@@ -184,6 +327,7 @@ function setup() {
     teal: color("#02dfc9"),
     blue: color("#293895"),
   };
+  PALETTE.darkish.setAlpha(0.75);
 
   BEKK_PALETTE = {
     // sort: color("#0E0E0E"),
@@ -207,152 +351,15 @@ function setup() {
     natt: color("#6D7ABB"),
     // natt_kontrast: color("#162365"),
   };
-  PALETTE.darkish.setAlpha(0.75);
-
-  BLOBS = [
-    new Blob({
-      z: 100,
-      cx: -width * 0.1,
-      cy: height * 0.9,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.3,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.yellow,
-    }),
-    new Blob({
-      z: 200,
-      cx: -width * 0.1,
-      cy: height * 0.1,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.45,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.pink,
-    }),
-    new Blob({
-      z: 300,
-      cx: 0,
-      cy: height * 0.5,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.2,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.dark,
-    }),
-    new Blob({
-      z: 400,
-      cx: width * 0.9,
-      cy: height,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.3,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.white,
-    }),
-    new Blob({
-      z: 500,
-      cx: width * 0.9,
-      cy: height * 0.8,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.3,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.yellow,
-    }),
-    new Blob({
-      z: 600,
-      cx: width * 1.2,
-      cy: height / 2,
-      rMin: vMin * 0.2,
-      rMax: vMin * 0.4,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.teal,
-    }),
-    new Blob({
-      z: 100,
-      cx: 0,
-      cy: 0,
-      rMin: vMin * 0.2,
-      rMax: vMin * 0.4,
-      aEnd: PI,
-      line: true,
-    }),
-    new Blob({
-      z: 200,
-      cx: width,
-      cy: height,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.3,
-      aStart: PI,
-      aEnd: PI + HALF_PI,
-      line: true,
-    }),
-    new Blob({
-      z: 300,
-      cx: width,
-      cy: height,
-      rMin: vMin * 0.15,
-      rMax: vMin * 0.35,
-      aStart: PI,
-      aEnd: PI + HALF_PI,
-      line: true,
-    }),
-    new Blob({
-      z: 3,
-      cx: width / 2,
-      cy: height / 2,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.25,
-      fillColor: RANDOM_COLORS ? randomColor() : PALETTE.white,
-    }),
-    new Blob({
-      z: 2,
-      cx: width / 2,
-      cy: height / 2,
-      rMin: vMin * 0.1,
-      rMax: vMin * 0.35,
-      fillColor: PALETTE.dark,
-    }),
-  ];
-
-  LINES = [];
-
-  SPOTS = [];
-
-  const SPOT_RMAX = vMin * 0.02;
-
-  for (let i = 0; i < SPOT_COUNT; i++) {
-    SPOTS.push(
-      new Blob({
-        z: i,
-        cx:
-          width * 0.2 +
-          random(-SPOT_RMAX * SPOT_SPREAD, SPOT_RMAX * SPOT_SPREAD) -
-          i * SPOT_SLANT,
-        cy: (i * SPOT_RMAX) / 2,
-        rMin: vMin * 0.005,
-        rMax: SPOT_RMAX,
-        fillColor: RANDOM_COLORS ? randomColor() : PALETTE.blue,
-        animationSpeed: 0,
-        steepness: 6,
-        disappearTime: 500,
-      })
-    );
-    SPOTS.push(
-      new Blob({
-        z: i + SPOT_COUNT,
-        cx:
-          width * 0.8 +
-          random(-SPOT_RMAX * SPOT_SPREAD, SPOT_RMAX * SPOT_SPREAD) +
-          i * SPOT_SLANT,
-        cy: height - (i * SPOT_RMAX) / 2,
-        rMin: vMin * 0.005,
-        rMax: SPOT_RMAX,
-        fillColor: RANDOM_COLORS ? randomColor() : PALETTE.blue,
-        animationSpeed: 0,
-        steepness: 6,
-        disappearTime: 500,
-      })
-    );
-  }
-
-  colorMode(HSL);
   BG = color(PALETTE.bg);
+
+  windowResized();
+  setupBlobs();
 
   textAlign(CENTER);
   textFont("Newzald");
-  textSize(TEXT_SIZE);
 
+  strokeWeight(STROKE_WEIGHT);
   noStroke();
   strokeCap(PROJECT);
 }
