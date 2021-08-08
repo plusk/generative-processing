20345f8fc0d38cb8c1df1fe29766d63124ec18e7
plusk <andelar96@gmail.com>
2021-08-08 16:19:36 +0200

    Neat, but bored now

--- sketches/v2/sketches/shadow.js
+++ sketches/v2/sketches/shadow.js
@@ -2,20 +2,29 @@ import palettes from "../palettes.js";
 import configBuilder, { baseSetup, ConfigValue } from "../utils.js";
 
 new p5((p) => {
-  let bg, colors, tickr, phase;
-  let coords = [];
+  let bg, colors;
 
   const c = configBuilder(p, {
     palette: "sirkul",
+    padding: new ConfigValue({ value: 0.25, min: 0, max: 1, step: 0.005 }),
     diameter: 50,
-    strokeWeight: 2,
     limit: 250,
-    outline: new ConfigValue({ value: 1, min: 1, max: 2, step: 0.05 }),
     fillAmp: new ConfigValue({ value: 1, min: 1, max: 1.25, step: 0.005 }),
     shadowAmp: new ConfigValue({ value: 1.025, min: 1, max: 1.25, step: 0.005 }),
-    outlineAmp: new ConfigValue({ value: 1, min: 1, max: 1.25, step: 0.005 }),
+    outline: false,
+    outlineAmp: new ConfigValue({ value: 1.05, min: 1, max: 1.25, step: 0.005 }),
+    outlineOffset: new ConfigValue({ value: 1.1, min: 1, max: 2, step: 0.05 }),
+    outlineWeight: 2,
     seed: p.random(10000),
   });
+
+  const getCoords = ({ r, a }, amp) => {
+    const ramp = r * amp;
+    return {
+      x: ramp * p.cos(a),
+      y: ramp * p.sin(a),
+    };
+  };
 
   p.setup = () => {
     baseSetup(p, c);
@@ -26,45 +35,40 @@ new p5((p) => {
     p.strokeWeight(c.diameter);
     p.noFill();
 
-    tickr = 0;
-    phase = 1;
-  };
-
-  const getCoords = ({ r, a }, amp) => {
-    const ramp = r * amp;
-    return {
-      x: ramp * p.cos(a),
-      y: ramp * p.sin(a),
-    };
-  };
-
-  p.draw = () => {
+    /* Now for the actual action */
+    let coords = [];
     p.translate(p.width / 2, p.height / 2);
-    const rmax = p.max(p.width, p.height) / 2;
-    if (phase === 1) {
+    const rmax = (p.max(p.width, p.height) / 2) * (1 - c.padding);
+
+    /* Shadow */
+    p.stroke("#222");
+    for (let i = 0; i < c.limit; i++) {
       coords.push({
         r: p.random(-rmax, rmax),
         a: p.random(p.TAU),
       });
-      const { x, y } = getCoords(coords[tickr], c.shadowAmp);
-      p.stroke("#222");
-      p.point(x, y);
-    } else if (phase === 2) {
-      p.strokeWeight(c.strokeWeight);
-      p.stroke(colors[tickr % colors.length]);
-      const { x, y } = getCoords(coords[tickr], c.outlineAmp);
-      p.circle(x, y, c.diameter * c.outline);
-    } else if (phase === 3) {
-      p.strokeWeight(c.diameter);
-      p.stroke(colors[tickr % colors.length]);
-      const { x, y } = getCoords(coords[tickr], c.fillAmp);
+      const { x, y } = getCoords(coords[i], c.shadowAmp);
       p.point(x, y);
     }
-    if (tickr === c.limit) {
-      phase++;
-      tickr = 0;
-    } else {
-      tickr++;
+
+    /* Outline */
+    if (c.outline) {
+      p.strokeWeight(c.outlineWeight);
+      for (let i = 0; i < c.limit; i++) {
+        p.stroke(colors[i % colors.length]);
+        const { x, y } = getCoords(coords[i], c.outlineAmp);
+        p.circle(x, y, c.diameter * c.outlineOffset);
+      }
+    }
+
+    /* Fill */
+    p.noStroke();
+    p.drawingContext.shadowBlur = 2;
+    p.drawingContext.shadowColor = "black";
+    for (let i = 0; i < c.limit; i++) {
+      p.fill(colors[i % colors.length]);
+      const { x, y } = getCoords(coords[i], c.fillAmp);
+      p.circle(x, y, c.diameter);
     }
   };
 });
